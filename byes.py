f429971bcae002db58541c3c1cd7a50d21572aa9
ning <1176265190@qq.com>
2020-12-09 23:32:55 +0800

    test

---
+++ byes.py
@@ -0,0 +1,80 @@
+# _*_ coding:utf-8 _*_
+from numpy import *
+import re
+import random
+
+def loadDataSet(): #创建样例数据
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]  #1代表脏话
+    return postingList, classVec
+
+def createVocabList(dataSet):  #创建词库 这里就是直接把所有词去重后，当作词库
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):  #文本词向量。词库中每个词当作一个特征，文本中有词，该词特征就是1，没有就是0
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs) #计算文本分类为脏话的先验概率，P(C)=3/6=1/2
+
+    #生成两个初始值全为1，长度为训练数据属性数量的数组
+    #防止某个类别计算出的概率为0，导致最后相乘都为0，所以初始词都赋值1，分母赋值为2.
+    p0Num = ones(numWords) 
+    p1Num = ones(numWords)
+    p0Denom = 2
+    p1Denom = 2
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+
+    #计算P(x_i│c)
+    p1Vect = log(p1Num / p1Denom)  #这里使用了Log取对数函数，方便计算，因为最后是比较大小，所有对结果没有影响。
+    p0Vect = log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1): #比较概率大小进行判断，
+    p1 = sum(vec2Classify*p1Vec)+log(pClass1)
+    p0 = sum(vec2Classify*p0Vec)+log(1-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet() #加载训练数据
+    myVocabList = createVocabList(listOPosts) #创建词库
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))# 把训练集的文本数据转换为矩阵形式
+    p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
+
+    testEntry = ['love', 'my', 'dalmation'] # 测试数据
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry = ['stupid', 'garbage'] # 测试数据
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+
+if __name__=='__main__':
+    testingNB()
